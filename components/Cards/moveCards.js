d1d3a134fb22beaa12c89279b879bc9c67904090
palak jaiswal <palakjaiswalch28@gmail.com>
2023-09-29 19:51:56 +0530

    added cards

---
+++ components/Cards/moveCards.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import styles from './card.module.css';
+export default function MoveCard(){
+    return <>
+        <div className={styles.moveparent}>
+        <div className={styles.move}>
+        Move Cards Here
+        </div>
+        </div>
+    </>
+}
---
+++ components/Todos/todos.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Card from "../Cards/card";
+import {AiFillPlusSquare} from 'react-icons/ai';
+import styles from './todos.module.css';
+import MoveCard from "../Cards/moveCards";
+export default function Todos(){
+    return <>
+       <div className={styles.Parent}>
+          <div className={styles.child}>
+          <div className={styles.top}>
+          <p className={styles.name}>To do List</p>
+          <p className={styles.add}><AiFillPlusSquare/></p>
+          </div>
+            
+            <div>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+          </div>
+          <div className={styles.child}>
+          <div className={styles.top}>
+          <p className={styles.name}>In Progress</p>
+          <p className={styles.addsp}><AiFillPlusSquare/></p>
+          </div>
+            <div>
+                <Card/>
+                <Card/>
+                
+            </div>
+          </div>
+          <div className={styles.child}>
+          <div className={styles.top}>
+          <p className={styles.name}>Done</p>
+          <p className={styles.addsp}><AiFillPlusSquare/></p>
+          </div>
+            <div>
+                <Card/>
+                <Card/>
+                <Card/>
+            </div>
+          </div>
+          <div className={styles.child}>
+          <div className={styles.top}>
+          <p className={styles.name}>Revised</p>
+          <p className={styles.addsp}><AiFillPlusSquare/></p>
+          </div>
+            <div>   
+            <MoveCard/>      
+            </div>
+          </div>
+          </div>
+       
+    </>
+}
